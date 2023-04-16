1b103d88953c915c3cd3330e4676f1f1d1429065
porridgewithraisins <santhanambr2002@gmail.com>
2023-04-16 21:22:21 +0530

    finish

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ num1 = st.number_input("Enter number 1", value=0)
 num2 = st.number_input("Enter number 2", value=0)
 num3 = st.number_input("Enter number 3", value=0)
 
-largest_number = max(num1, num2, num3)
-
 if num1 == num2 == num3:
     st.write("All three numbers are equal.")
 else:
-    st.write("The largest number is:", largest_number)
+    st.write("The largest number is:", max(num1, num2, num3))
